01223b963da55e12fee6af98cb279c14e08e61e1
anheejeong <eyrt6973@naver.com>
2024-11-11 21:36:29 +0900

    [Chore] 초기 환경 세팅

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+import js from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
+import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+      prettier: prettierPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/function-component-definition': [
+        2,
+        { namedComponents: ['arrow-function', 'function-declaration'] },
+      ],
+      'import/prefer-default-export': 'off',
+      'react/require-default-props': 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          '': 'never',
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': [
+        'error',
+        { variables: false, functions: false, classes: false },
+      ],
+      'react/jsx-props-no-spreading': 'off',
+      'react/no-unstable-nested-components': 'off',
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  }
+)
