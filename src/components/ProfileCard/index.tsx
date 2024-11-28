58728975db9d753b4f1beaafde8865658beae76a
anheejeong <eyrt6973@naver.com>
2024-11-28 10:36:32 +0900

    [Design] 프로필 카드 디자인 수정

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -4,38 +4,36 @@ export const ProfileCard = ({
   name,
   photo,
   email,
-  authType,
   buttonMod,
   onButtonClick,
 }: ProfileCardItem) => {
-  const authContent = authType === 'M' ? 'Manager' : 'Employee'
-
   return (
     <div
       className="flex flex-col rounded-md justify-end m-2 overflow-hidden"
       style={{
-        width: '220px',
-        height: '170px',
+        width: '180px',
+        height: '160px',
         backgroundImage: `url(${photo})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
     >
-      <div className="flex bg-components-profileCard bg-opacity-80 justify-between px-3 py-2">
-        <div className="flex flex-col text-white">
-          <div className="flex align-end">
-            <div className="text-xl font-semibold">{name}</div>
-            <div className="text-xs">{authContent}</div>
+      <div className="flex bg-black-600 bg-opacity-80 justify-between px-3 py-2">
+        <div className="flex flex-col text-white w-full">
+          <div className="flex justify-between">
+            <div className="flex">
+              <div className="text-xl font-semibold">{name}</div>
+            </div>
+            <div>
+              <button
+                onClick={onButtonClick}
+                className="bg-white px-1.5 rounded-xl text-xs text-text-default"
+              >
+                {buttonMod}
+              </button>
+            </div>
           </div>
           <div className="text-sm">{email}</div>
-        </div>
-        <div>
-          <button
-            onClick={onButtonClick}
-            className="bg-white px-2 rounded-xl text-sm"
-          >
-            {buttonMod}
-          </button>
         </div>
       </div>
     </div>
