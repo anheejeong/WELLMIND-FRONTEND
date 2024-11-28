ebd737bccc886f1b1d4294c8e57f583a830bb784
anheejeong <eyrt6973@naver.com>
2024-11-28 17:30:33 +0900

    [Feat] 초기 store 세팅 및 authToken 세팅

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import authTokenSlice from '@/store/slices/authToken'
+
+export const store = configureStore({
+  reducer: {
+    authToken: authTokenSlice,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
