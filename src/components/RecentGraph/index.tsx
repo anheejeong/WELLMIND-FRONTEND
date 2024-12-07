892b52f6f339a11e39ac151505712b5180d7c7ec
anheejeong <eyrt6973@naver.com>
2024-12-07 20:26:33 +0900

    [Refactor] 컴포넌트형으로 리팩토링

--- src/components/RecentGraph/index.tsx
+++ src/components/RecentGraph/index.tsx
@@ -1,84 +1,80 @@
 import dottedLine from '@/assets/dottedLine.svg'
 
-export default function RecentGraph() {
+const COLORS = [
+  'bg-default-lightGreen',
+  'bg-primary-200',
+  'bg-default-darkGray',
+  'bg-default-orange',
+  'bg-default-red',
+  'bg-black-400',
+]
+
+type DataType = {
+  text: string
+}
+
+type RecentGraphProps = {
+  data: DataType[]
+}
+
+export default function RecentGraph({ data }: RecentGraphProps) {
+  const itemWidth = `${100 / data.length}%`
+
   return (
     <div className="w-full h-full flex justify-center items-center gap-1 mt-12">
-      <div className="w-1/6 flex flex-col justify-center items-center text-center">
-        <div className="font-semibold w-24 h-16 bg-default-lightGreen text-text-white rounded-full p-2">
-          지각, 야근 빈발형
-        </div>
-        <img className="h-10" src={dottedLine} alt="dottedLine" />
-        <div className="w-full h-6 bg-default-lightGreen rounded-l-lg" />
-        <div className="h-10" />
-        <div className="font-semibold text-text-default w-20 h-16" />
-      </div>
+      {data.map((item, index) => {
+        const color = COLORS[index % COLORS.length]
 
-      <div className="w-1/6 flex flex-col justify-center items-center text-center">
-        <div className="font-semibold text-text-default w-20 h-16" />
-        <div className="h-10" />
-        <div className="w-full h-6 bg-primary-200" />
-        <img className="h-10" src={dottedLine} alt="dottedLine" />
-        <div className="font-semibold w-24 h-16 bg-primary-200 text-text-white rounded-full p-2">
-          지각, 야근 빈발형
-        </div>
-      </div>
+        const isFirst = index === 0
+        const isLast = index === data.length - 1
+        const roundedClass = `${isFirst ? 'rounded-l-lg' : ''} ${isLast ? 'rounded-r-lg' : ''}`
 
-      <div className="w-1/6 flex flex-col justify-center items-center text-center">
-        <div className="font-semibold w-24 h-16 bg-default-darkGray text-text-white rounded-full p-2">
-          지각, 야근 빈발형
-        </div>
-        <img className="h-10" src={dottedLine} alt="dottedLine" />
-        <div className="w-full h-6 bg-default-darkGray" />
-        <div className="h-10" />
-        <div className="font-semibold text-text-default w-20 h-16" />
-      </div>
+        const isEven = index % 2 !== 0
 
-      <div className="w-1/6 flex flex-col justify-center items-center text-center">
-        <div className="font-semibold text-text-default w-20 h-16" />
-        <div className="h-10" />
-        <div className="w-full h-6 bg-default-orange" />
-        <img className="h-10" src={dottedLine} alt="dottedLine" />
-        <div className="font-semibold w-24 h-16 bg-default-orange text-text-white rounded-full p-2">
-          지각, 야근 빈발형
-        </div>
-      </div>
-
-      <div className="w-1/6 flex flex-col justify-center items-center text-center">
-        <div className="font-semibold w-24 h-16 bg-default-red text-text-white rounded-full p-2">
-          지각, 야근 빈발형
-        </div>
-        <img className="h-10" src={dottedLine} alt="dottedLine" />
-        <div className="w-full h-6 bg-default-red" />
-        <div className="h-10" />
-        <div className="font-semibold text-text-default w-20 h-16" />
-      </div>
-
-      <div className="w-1/6 flex flex-col justify-center items-center text-center">
-        <div className="font-semibold text-text-default w-20 h-16" />
-        <div className="h-10" />
-        <div className="w-full h-6 bg-black-400 rounded-r-lg" />
-        <img className="h-10" src={dottedLine} alt="dottedLine" />
-        <div className="font-semibold w-24 h-16 bg-black-400 text-text-white rounded-full p-2">
-          지각, 야근 빈발형
-        </div>
-      </div>
+        return (
+          <div
+            key={index}
+            style={{ flexBasis: itemWidth }}
+            className="flex flex-col justify-center items-center text-center"
+          >
+            {!isEven ? (
+              <>
+                <div
+                  className={`flex flex-col justify-center items-center font-semibold text-text-white w-24 h-16 ${color} rounded-full p-2`}
+                >
+                  {item.text.split(' ').map((word, index) => (
+                    <span key={index}>{word}</span>
+                  ))}
+                </div>
+                <img className="h-10" src={dottedLine} alt="dottedLine" />
+              </>
+            ) : (
+              <>
+                <div className="h-16" />
+                <div className="h-10" />
+              </>
+            )}
+            <div className={`w-full h-6 ${color} ${roundedClass}`} />
+            {isEven ? (
+              <>
+                <img className="h-10" src={dottedLine} alt="dottedLine" />
+                <div
+                  className={`flex flex-col justify-center items-center font-semibold text-text-white w-24 h-16 ${color} rounded-full p-2`}
+                >
+                  {item.text.split(' ').map((word, index) => (
+                    <span key={index}>{word}</span>
+                  ))}
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="h-10" />
+                <div className="h-16" />
+              </>
+            )}
+          </div>
+        )
+      })}
     </div>
   )
 }
-
-// const COLORS = [
-//   'bg-default-lightGreen',
-//   'bg-primary-200',
-//   'bg-default-darkGray',
-//   'bg-default-orange',
-//   'bg-default-red',
-//   'bg-text-gray',
-// ]
-
-// type DataType = {
-//   text: string
-// }
-
-// type RecentGraphProps = {
-//   data: DataType[]
-// }
--- src/pages/MainPage/SlideMenu/RecentReport/index.tsx
+++ src/pages/MainPage/SlideMenu/RecentReport/index.tsx
@@ -3,7 +3,16 @@ import RecentGraph from '@/components/RecentGraph'
 export default function RecentReport() {
   return (
     <div className="w-full">
-      <RecentGraph />
+      <RecentGraph data={dummyData} />
     </div>
   )
 }
+
+const dummyData = [
+  { text: '정상 출퇴근형' },
+  { text: '지각 빈발형' },
+  { text: '야근 빈발형' },
+  { text: '출장 빈발형' },
+  { text: '조퇴 빈발형' },
+  { text: '외출 빈발형' },
+]
