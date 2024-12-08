61ee20daefa8b4bfa86886e03886e954066cf0c7
anheejeong <eyrt6973@naver.com>
2024-12-09 01:08:31 +0900

    [Feat] 로그인 API 연결

---
+++ src/api/services/auth/login.api.ts
@@ -0,0 +1,57 @@
+import { useNavigate } from 'react-router-dom'
+
+import { useMutation } from '@tanstack/react-query'
+
+import { fetchInstance } from '@/api/instance'
+import { LoginRequest } from '@/types'
+
+const postLogin = async ({ employeeId, password }: LoginRequest) => {
+  const response = await fetchInstance.post('/api/public/login', {
+    employeeId,
+    password,
+  })
+
+  if (response.status === 200) {
+    return true
+  } else if (response.status === 404) {
+    alert(
+      '아이디 또는 비밀번호가 잘못 되었습니다.\n아이디와 비밀번호를 정확히 입력해 주세요.'
+    )
+    return false
+  } else if (response.status === 409) {
+    alert('이미 로그인 된 계정입니다.')
+    return true
+  }
+}
+
+export const usePostLogin = () => {
+  const navigate = useNavigate()
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: (success) => {
+      if (success) navigate('/main')
+    },
+    onError: () => {},
+  })
+}
+
+// const postLogin = async ({ employeeId, password }: LoginRequest) => {
+//   const response = await fetchInstance.post('/api/public/login', {
+//     employeeId,
+//     password,
+//   })
+
+//   console.log(response)
+//   console.log(response.status)
+
+//   const accessToken = response.headers.authorization
+
+//   return accessToken
+// }
+
+// export const usePostLogin = ({ employeeId, password }: LoginRequest) => {
+//   return useQuery({
+//     queryKey: ['login', employeeId, password],
+//     queryFn: () => postLogin({ employeeId, password }),
+//   })
+// }
--- src/pages/AuthPage/Login/index.tsx
+++ src/pages/AuthPage/Login/index.tsx
@@ -1,7 +1,19 @@
-import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+
+import { usePostLogin } from '@/api/services/auth/login.api'
+import { LoginRequest } from '@/types'
 
 export default function Login() {
-  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginRequest>()
+  const { mutate } = usePostLogin()
+
+  const onSubmit = async (data: LoginRequest) => {
+    await mutate(data)
+  }
 
   return (
     <div className="flex flex-col w-1/3 min-w-64 gap-4 rounded-md shadow-xl p-8 border border-slate-100 bg-white">
@@ -9,26 +21,39 @@ export default function Login() {
         <div className="text-2xl font-semibold">환영합니다</div>
         <div className="">효율적인 인사 관리 시스템</div>
       </div>
-      <div>
-        <label className="text-lg text-text-default">사번</label>
-        <input className="w-full text-xl rounded-md border-2 border-solid border-text-light focus:border-text-secondary focus:outline-none px-2 py-1" />
-      </div>
-      <div>
-        <label className="text-lg text-text-default">비밀번호</label>
-        <input
-          type="password"
-          className="w-full text-xl rounded-md border-2 border-solid border-text-light focus:border-text-secondary focus:outline-none px-2 py-1"
-        />
-        <div className="w-full flex justify-end text-text-darkgray pt-1">
-          <button>비밀번호 찾기</button>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+        <div>
+          <label className="text-lg text-text-default">사번</label>
+          <input
+            type="text"
+            className="w-full text-xl rounded-md border-2 border-solid border-text-light focus:border-text-secondary focus:outline-none px-2 py-1"
+            {...register('employeeId', { required: '사번은 필수입니다.' })}
+          />
+          {errors.employeeId && (
+            <p className="text-red-500 text-sm">{errors.employeeId.message}</p>
+          )}
         </div>
-      </div>
-      <button
-        className="text-white text-xl font-semibold rounded-xl bg-primary-800 py-1 my-3 hover:bg-primary-700 focus:bg-primary-800"
-        onClick={() => navigate('/main')}
-      >
-        Login
-      </button>
+        <div>
+          <label className="text-lg text-text-default">비밀번호</label>
+          <input
+            type="password"
+            className="w-full text-xl rounded-md border-2 border-solid border-text-light focus:border-text-secondary focus:outline-none px-2 py-1"
+            {...register('password', { required: '비밀번호는 필수입니다.' })}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
+          <div className="w-full flex justify-end text-text-darkgray pt-1">
+            <button>비밀번호 찾기</button>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="text-white text-xl font-semibold rounded-xl bg-primary-800 py-1 my-3 hover:bg-primary-700 focus:bg-primary-800"
+        >
+          Login
+        </button>
+      </form>
     </div>
   )
 }
