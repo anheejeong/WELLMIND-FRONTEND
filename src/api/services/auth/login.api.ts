cd5fed24af79276729bbefc46df44b807d5cec88
anheejeong <eyrt6973@naver.com>
2024-12-09 19:22:18 +0900

    [Feat] 로그인 API store 저장

--- src/api/services/auth/login.api.ts
+++ src/api/services/auth/login.api.ts
@@ -3,16 +3,26 @@ import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
 
 import { fetchInstance } from '@/api/instance'
+import { useAppDispatch } from '@/store/hooks/useAppDispatch'
+import { setAuthToken } from '@/store/slices/authToken'
 import { LoginRequest } from '@/types'
 
-const postLogin = async ({ employeeId, password }: LoginRequest) => {
+const postLogin = async ({
+  employeeId,
+  password,
+}: LoginRequest): Promise<
+  boolean | { accessToken: string; refreshToken: string }
+> => {
   const response = await fetchInstance.post('/api/public/login', {
     employeeId,
     password,
   })
 
   if (response.status === 200) {
-    return true
+    return {
+      accessToken: response.data.data.accessToken,
+      refreshToken: response.data.data.refreshToken,
+    }
   } else if (response.status === 404 || response.status === 400) {
     alert(
       '아이디 또는 비밀번호가 잘못 되었습니다.\n아이디와 비밀번호를 정확히 입력해 주세요.'
@@ -20,16 +30,34 @@ const postLogin = async ({ employeeId, password }: LoginRequest) => {
     return false
   } else if (response.status === 409) {
     alert('이미 로그인 된 계정입니다.')
-    return true
+    return false
   }
+
+  return false
 }
 
 export const usePostLogin = () => {
+  const dispatch = useAppDispatch()
   const navigate = useNavigate()
   return useMutation({
     mutationFn: postLogin,
-    onSuccess: (success) => {
-      if (success) navigate('/main')
+    onSuccess: (
+      data: { accessToken?: string; refreshToken?: string } | boolean
+    ) => {
+      if (
+        data &&
+        typeof data === 'object' &&
+        data.accessToken &&
+        data.refreshToken
+      ) {
+        dispatch(
+          setAuthToken({
+            accessToken: data.accessToken,
+            refreshToken: data.refreshToken,
+          })
+        )
+        navigate('/main')
+      }
     },
     onError: () => {},
   })
