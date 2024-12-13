0ffa4edb87c49c20b32a4ea19dd4ade6bf44c4d7
anheejeong <eyrt6973@naver.com>
2024-12-13 22:09:11 +0900

    [Feat] 그래프 파라미터 변경

--- src/pages/MainPage/SlideMenu/CommuteGraph/index.tsx
+++ src/pages/MainPage/SlideMenu/CommuteGraph/index.tsx
@@ -1,32 +1,15 @@
 import { Graph } from '@/components/Graph'
 
-export default function CommuteGraph() {
+type CommuteGraphTypes = {
+  time: string[]
+  date: string[]
+}
+
+export default function CommuteGraph({ time, date }: CommuteGraphTypes) {
   return (
     <div className="flex py-1 mx-20 rounded-md align-center bg-background-light">
       <div className="mx-auto">
-        <Graph
-          type="big"
-          time={[
-            '09:00',
-            '09:00',
-            '09:20',
-            '08:50',
-            '09:00',
-            '09:02',
-            '08:20',
-            '08:98',
-          ]}
-          date={[
-            '2024-11-19',
-            '2024-11-20',
-            '2024-11-21',
-            '2024-11-22',
-            '2024-11-23',
-            '2024-11-24',
-            '2024-11-25',
-            '2024-11-26',
-          ]}
-        />
+        <Graph type="big" time={time} date={date} />
       </div>
     </div>
   )
