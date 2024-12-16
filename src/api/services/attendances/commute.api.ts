0e1caa5eaa069ba6b3d684519925c69592511709
anheejeong <eyrt6973@naver.com>
2024-12-16 21:24:48 +0900

    [Feat] 출퇴근 기록 api 연결

---
+++ src/api/services/attendances/commute.api.ts
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { authorizationInstance } from '@/api/instance'
+import { CommuteResponseParams, PagingRequestParams } from '@/types'
+
+type AttendRequest = {
+  employeeId?: string
+} & PagingRequestParams
+
+const getAttend = async (params: AttendRequest) => {
+  const response = await authorizationInstance.get<CommuteResponseParams>(
+    `/api/attend/find-by`,
+    {
+      params,
+    }
+  )
+
+  return {
+    content: response.data.data.content,
+    totalPages: response.data.data.totalPages,
+    totalElements: response.data.data.totalElements,
+  }
+}
+
+export const useGetAttend = (options: Partial<AttendRequest> = {}) => {
+  const { page = 0, size = 10, sort = [], employeeId } = options
+
+  return useQuery({
+    queryKey: ['attend', page, size, sort, employeeId],
+    queryFn: () => getAttend({ page, size, sort, employeeId }),
+  })
+}
