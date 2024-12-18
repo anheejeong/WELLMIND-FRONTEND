6cdecb7a60c8827202449ee154364b99d4872447
anheejeong <eyrt6973@naver.com>
2024-12-18 20:40:18 +0900

    [Feat] 새 직원 추가 api 구현

---
+++ src/api/services/admin/newEmployee.api.ts
@@ -0,0 +1,31 @@
+import { authorizationInstance } from '@/api/instance'
+import { NewEmployeeItem } from '@/types'
+
+// type PostNewEmployeeType = {
+//   info: NewEmployeeItem
+// }
+
+export const postNewEmployee = async ({
+  authType,
+  departName,
+  email,
+  employeeId,
+  name,
+  phoneNum,
+  positionName,
+}: NewEmployeeItem) => {
+  const response = await authorizationInstance.post(
+    '/api/auth/register/profile',
+    {
+      authType,
+      departName,
+      email,
+      employeeId,
+      name,
+      phoneNum,
+      positionName,
+    }
+  )
+
+  return response.data.message
+}
