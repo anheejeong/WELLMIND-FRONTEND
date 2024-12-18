2669c6c99964df9418c7083803a1185aaadfe9cc
anheejeong <eyrt6973@naver.com>
2024-12-18 16:26:09 +0900

    [Feat] 직원 리스트 조회 api 구현

---
+++ src/api/services/admin/allEmployees.api.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { authorizationInstance } from '@/api/instance'
+import { SimpleEmployeeType } from '@/types'
+
+const getAllEmployees = async () => {
+  const response =
+    await authorizationInstance.get<SimpleEmployeeType>('/api/auth/find-by')
+
+  console.log(response.data.content)
+
+  return response.data.content
+}
+
+export const useGetAllEmployees = () => {
+  return useQuery({
+    queryKey: ['allEmployees'],
+    queryFn: () => getAllEmployees(),
+  })
+}
